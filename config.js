39945c5af13fdfda7995d32beb10590ad9583b07
João Gabriel Beck <jgdbeck@gmail.com>
2026-08-17 13:58:00 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -9,5 +9,5 @@
    SHEET_API com a  -> MODO CONECTADO: dados numa Planilha Google, iguais para todos.
    URL do /exec        Leitura é pública; para editar é preciso a senha da equipe. */
 const CONFIG = {
-  SHEET_API: ""   // ex.: "https://script.google.com/macros/s/AKfy.../exec"
+  SHEET_API: ""   // ex.: "https://script.google.com/macros/s/AKfycbz3fkfT9gZY3uKa2r97rHEkuMhjoKqH3oNsavguhKaFwOhgwJVdxiD1VhJnZBPPz03U/exec"
 };
